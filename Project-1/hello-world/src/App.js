603b067f9f04be5788f37e8210df3944e0728bae
Bharat Poptwani <b4bharat.available@gmail.com>
2018-01-10 09:07:50 +0530

    Added the Intro Component into a separate file

--- Project-1/hello-world/src/App.js
+++ Project-1/hello-world/src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./Header";
-// import Intro from "./Intro";
-
-class Intro extends Component {
-  render() {
-    return (
-      <div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Intro from "./Intro";
 
 class App extends Component {
   render() {
