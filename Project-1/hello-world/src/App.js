18e07e73ab1520703163605c2c44cae64061a5a0
Bharat Poptwani <b4bharat.available@gmail.com>
2018-01-10 08:54:58 +0530

    created a separate 'Intro' component

--- Project-1/hello-world/src/App.js
+++ Project-1/hello-world/src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./Header";
+// import Intro from "./Intro";
+
+class Intro extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
@@ -8,11 +21,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <p> Hey </p>
-        <div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <Intro />
       </div>
     );
   }
