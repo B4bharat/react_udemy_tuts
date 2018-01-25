3d1124d1c662c1b21d9f1839cfb7fdbc920332fa
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2018-01-25 19:38:11 +0530

    minor changes into the main app

--- Project-2/component-basic/src/App.js
+++ Project-2/component-basic/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.yourname = 'ammy';
+    this.state = {};
+  }
+
+  sayHello(name) {
+    return `Hello ${name}`;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Just some sample data: {this.yourname} </h2>
       </div>
     );
   }
