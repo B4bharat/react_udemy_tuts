7730cfc96d1ddbd9602c9dbcb9aae412e777b0a7
Bharat Poptwani <b4bharat.available@gmail.com>
2018-01-28 16:27:48 +0530

    Explained react props using multiple parent child components

--- Project-3/props-state/src/App.js
+++ Project-3/props-state/src/App.js
@@ -6,82 +6,52 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <h3>prop number is : {this.props.propNumber}</h3>
-        <h3>prop number is : {this.props.propString}</h3>
-        <h3>prop number is : {this.props.propObject.obj1}</h3>
         <Parent />
       </div>
     );
   }
 }
 
-App.propTypes = {
-  propObject: React.PropTypes.object,
-  propString: React.PropTypes.string,
-  propNumber: React.PropTypes.number
-}
-
-App.defaultProps = {
-  propNumber: 3,
-  propString: "THis is prop string",
-  propObject: {
-    obj1: "I am obj 1",
-    obj2: "I am obj 2",
-    obj3: "I am obj 3"
-  }
-
-}
-
-
-
-
 class Parent extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      cars: ['s-BMW', 's-MERC', 's-City', 's-Audi']
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(){
-    this.setState( {cars: this.state.cars.reverse()});
-  }
-
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <h2 onClick={this.handleClick}>Just some info</h2>
-        <Cars msg="cars are cool" model="34765" coolCars={this.state.cars}/>
+        <h2>Just some info</h2>
+        <Cars
+          msg="cars are cool"
+          model={3467}
+          coolCars={this.props.cars}
+        />
       </div>
     );
   }
 }
 
-
 Parent.defaultProps = {
-  cars: ['BMW', 'MERC', 'City', 'Audi']
-}
+  cars: ['BMW', 'MERC', 'CITY', 'Audi']
+};
 
 class Cars extends Component {
-  render(){
-    console.log(this.props);
-    return(
+  render() {
+    return (
       <div>
-        <h3> I am from cars component</h3>
+        <h3>I am from cars component</h3>
         <p>{this.props.msg}</p>
         <p>{this.props.model}</p>
-        <div>{this.props.coolCars.map((item, i) => {
-            return <p key={i}>{item}</p> ;
-          })}</div>
+        <ul>
+          {
+            this.props.coolCars.map((car, i) => {
+              return <li>{car}</li>;
+            })
+          }
+        </ul>
       </div>
     );
   }
